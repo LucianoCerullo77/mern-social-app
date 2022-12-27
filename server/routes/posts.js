cc58e61c00824e001deecdf149c06df69355aa21
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-27 13:10:57 -0300

    posts routes maded

---
+++ server/routes/posts.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { getfeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// READ
+
+router.get("/", verifyToken, getfeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// UPDATE
+
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
