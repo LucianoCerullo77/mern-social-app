c42796ed7b3a575045e9fc786eda9a759d6d0620
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-29 12:57:36 -0300

    routes maded

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from './pages/homePage'
+import LoginPage from './pages/loginPage'
+import ProfilePage from './pages/profilePage'
+ 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/Home" element={<HomePage/>}/>
+        <Route path="/Profile" element={<ProfilePage/>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
