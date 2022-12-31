5aa348a15a0716dc38579a411af6bc90c3d760b3
Luciano Cerullo <lucianonicolas10@gmail.com>
2022-12-31 07:38:01 -0300

    form started

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -17,31 +17,84 @@ import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 
 const registerSchema = yup.object().shape({
-    firstName : yup.string().required("required"),
-    lastName:yup.string().required("required"),
-    email:yup.string().email("invalid email").required("required"),
-    password:yup.string().required("required"),
-    occupation:yup.string().required("required"),
-    picture:yup.string().required("required"),
-})
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+  occupation: yup.string().required("required"),
+  picture: yup.string().required("required"),
+});
 
 const loginSchema = yup.object().shape({
-    email:yup.string().email("invalid email").required("required"),
-    password:yup.string().required("required"),
-})
+  email: yup.string().email("invalid email").required("required"),
+  password: yup.string().required("required"),
+});
 
 const initialValuesRegister = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    occupation: "",
-    picture: "",
-}
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  occupation: "",
+  picture: "",
+};
 
 const initialValuesLogin = {
-    email: "",
-    password: "",
-}
+  email: "",
+  password: "",
+};
 
+const Form = () => {
+  const [pageType, setPageType] = useState("login");
+  const { palette } = useTheme();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const isLogin = pageType == "login";
+  const isRegister = pageType == "register";
 
+  const handleFormSubmit = async (values, onSubmitProps) => {};
+  return (
+    <Formik
+      onSubmit={handleFormSubmit}
+      initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
+      validationSchema={isLogin ? loginSchema : registerSchema}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm,
+      }) => (<form onSubmit={handleSubmit}>
+        <Box
+        display="grid"
+        gap="30px"
+        gridTemplateColumns="repeat(4,minmax(0,1fr))"
+        sx={{
+            "& > div" : {gridColumn: isNonMobile ? undefined : "span 4"}
+        }}
+        >
+        {isRegister && (
+            <>
+                <TextField
+                    label="First Name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.firstName}
+                    name="firstName"
+                    error={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                />
+            </>
+        )}
+
+        </Box>
+      </form>)}
+    </Formik>
+  );
+};
+
+export default Form;
