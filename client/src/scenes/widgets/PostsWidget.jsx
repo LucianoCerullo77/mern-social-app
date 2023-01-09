1f602481c8e72640590495d894af10c41ec8cdac
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-09 10:08:48 -0300

    userPosts maded

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -16,6 +16,15 @@ const PostsWidget = ({ userId, isProfile = false }) => {
     const data = await response.json();
     dispatch(setPosts({ posts: data }));
   };
+
+  const getUserPosts = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${userId}/posts`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
 };
 
 export default PostsWidget;
