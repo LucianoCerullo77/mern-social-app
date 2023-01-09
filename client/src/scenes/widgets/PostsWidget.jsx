38ef94a6ca69365f4c0cc8cd57c29df3bea3a1a2
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-09 10:01:13 -0300

    postswidget started

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({ userId, isProfile = false }) => {};
+
+export default PostsWidget;
