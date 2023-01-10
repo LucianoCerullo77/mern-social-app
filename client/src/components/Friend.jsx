e1e4c8ebd27d1addd00e47f3a3741840e8a2f8ce
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-10 10:08:49 -0300

    constants defined

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -7,13 +7,16 @@ import FlexBetween from "./FlexBetween";
 import UserImage from "./UserImage";
 
 const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const {_id} = useSelector((state) => state.user)
-    const token = useSelector((state) => state.token)
-    const friends = useSelector((state) => state.user.friends)
-    const {palette} = useTheme()
-    const primaryLight = palette.primary.light
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const friends = useSelector((state) => state.user.friends);
+  const { palette } = useTheme();
+  const primaryLight = palette.primary.light;
+  const primaryDark = palette.primary.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
 };
 
 export default Friend;
