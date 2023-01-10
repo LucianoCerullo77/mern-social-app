2be28c967f0986a9a35439acfbbddb3d80ce6c2e
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-10 10:01:47 -0300

    friend component started

---
+++ client/src/components/Friend.jsx
@@ -0,0 +1,12 @@
+import { PersonAddOutlined, PersonRemoveOutlined } from "@mui/icons-material";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { setFriends } from "state";
+import FlexBetween from "./FlexBetween";
+import UserImage from "./UserImage";
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+    
+};
+
+export default Friend;
