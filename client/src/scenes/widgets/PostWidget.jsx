deb54a44be9ef3fd8523e82e7e3184ad0c4f1be6
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-12 11:58:49 -0300

    like button updated

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -77,6 +77,7 @@ const PostWidget = ({
                 <FavoriteBorderOutlines />
               )}
             </IconButton>
+            <Typography>{likeCount}</Typography>
           </FlexBetween>
         </FlexBetween>
       </FlexBetween>
