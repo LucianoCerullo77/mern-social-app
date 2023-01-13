251903f3ef439eee4a9b780872c3b65a7a754472
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-13 10:48:25 -0300

    advert updated

--- client/src/scenes/widgets/AdvertWidget.jsx
+++ client/src/scenes/widgets/AdvertWidget.jsx
@@ -1,14 +1,30 @@
-import { Typography,useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
 const AdvertWidget = () => {
-    const {palette} = useTheme()
-    const dark = palette.neutral.dark
-    const main = palette.neutral.main
-    const medium = palette.neutral.medium
+  const { palette } = useTheme();
+  const dark = palette.neutral.dark;
+  const main = palette.neutral.main;
+  const medium = palette.neutral.medium;
 
-    return
-}
+  return (
+    <WidgetWrapper>
+      <FlexBetween>
+        <Typography color={dark} variant="h5" fontWeight="500">
+          Sponsored
+        </Typography>
+        <Typography color={medium}>Create AD</Typography>
+      </FlexBetween>
+      <img
+        width="100%"
+        height="auto"
+        alt="advert"
+        src="http://localhost:3001/assets/info4.jpeg"
+        style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
+      />
+    </WidgetWrapper>
+  );
+};
 
-export default AdvertWidget
+export default AdvertWidget;
