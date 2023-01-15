1d2815a6a9683950fd27e616615b345af76f4ba8
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-15 10:18:46 -0300

    profile updated

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -8,8 +8,13 @@ import MyPostWidget from "scenes/widgets/MyPostWidget";
 import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
-const index = () => {
+const ProfilePage = () => {
+  const [user,setUser] = useState(null)
+  const {userId} = useParams()
+  const token = useSelector((state) => state.token)
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
+
   return <div>Profile page</div>;
 };
 
-export default index;
+export default ProfilePage;
