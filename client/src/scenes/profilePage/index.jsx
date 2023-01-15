1ded8b1edea940cc02a103b70d8ef42ea3b7e986
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-15 10:25:45 -0300

    fetch call maded in profile

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -9,10 +9,19 @@ import PostsWidget from "scenes/widgets/PostsWidget";
 import UserWidget from "scenes/widgets/UserWidget";
 
 const ProfilePage = () => {
-  const [user,setUser] = useState(null)
-  const {userId} = useParams()
-  const token = useSelector((state) => state.token)
-  const isNonMobileScreens = useMediaQuery("(min-width:1000px)")
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
 
   return <div>Profile page</div>;
 };
