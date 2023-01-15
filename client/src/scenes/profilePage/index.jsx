f6b7797734e343b6ffe1e80bb7f0674163598158
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-01-15 10:15:20 -0300

    profile page started

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const index = () => {
-  return (
-    <div>Profile page</div>
-  )
-}
+  return <div>Profile page</div>;
+};
 
-export default index
+export default index;
